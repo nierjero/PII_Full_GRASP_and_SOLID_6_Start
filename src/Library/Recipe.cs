769108d3c38096902001fbd3aaf3f0984d934a2f
nierjero <jeronimo.gonzalez@correo.ucu.edu.uy>
2023-11-15 09:39:44 -0300

    fin

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -6,6 +6,8 @@
 
 using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 
 namespace Full_GRASP_And_SOLID
 {
@@ -15,6 +17,8 @@ namespace Full_GRASP_And_SOLID
         private IList<BaseStep> steps = new List<BaseStep>();
 
         public Product FinalProduct { get; set; }
+
+        public bool Cooked { get; private set; } = false;
 
         // Agregado por Creator
         public void AddStep(Product input, double quantity, Equipment equipment, int time)
@@ -61,6 +65,40 @@ namespace Full_GRASP_And_SOLID
             }
 
             return result;
+        }
+
+         public class RecipeTimerClient : TimerClient
+        {
+            private Recipe recipe;
+
+            public RecipeTimerClient(Recipe recipe)
+            {
+            this.recipe = recipe;
+            }
+
+            public void TimeOut()
+            {
+                this.recipe.Cooked = true;
+            }
+            
+    }
+       public void Cook()
+        {
+           int TotalTime = GetCookTime();
+           CountdownTimer timer = new CountdownTimer();
+           RecipeTimerClient timerClient = new RecipeTimerClient(this);
+           timer.Register(TotalTime, timerClient);
+        }
+
+        public int GetCookTime()
+        {
+            int CookTime = 0;
+            foreach (BaseStep step in this.steps)
+            {
+                
+                CookTime = CookTime + step.Time;
+            }
+            return CookTime;
         }
     }
 }
